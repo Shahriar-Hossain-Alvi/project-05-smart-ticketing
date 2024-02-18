c73b18ec2934bda61cbc964678d7998c61b49be8
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-02-18 23:38:14 +0600

    make append child function

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,7 @@ bannerBtn.addEventListener('click', function(){
 const seatLayoutContainer = document.getElementById('seat-layout-container');
 const selectedSeatCounter = document.getElementById('selected-seat-counter')
 const seatLeftId = document.getElementById('seat-left');
+
 const seatID = seatLayoutContainer.querySelectorAll('h2');
 let selectedSeatCounterUpdater = 0;
 let seatLeft = 40;
@@ -17,20 +18,56 @@ let seatLeft = 40;
 for(let i=0; i<seatID.length; i++){
     const seats = seatID[i];
     let flag = false;
+
+    // on single click
     seats.addEventListener('click', function(){
+        //change color
         seats.style.backgroundColor = '#1DD100';
+
+        //do not update counter if already clicked
         if(flag === true){
             selectedSeatCounterUpdater = selectedSeatCounterUpdater;
             seatLeft = seatLeft;
         }
         else{
+            //update counter on clicked
             selectedSeatCounterUpdater++;
             selectedSeatCounter.innerText = selectedSeatCounterUpdater;
             seatLeft--;
             seatLeftId.innerText = seatLeft;
+
+            //append seats in the side menu
+            const selectedSeatList = document.getElementById('selected-seat-list');
+            const p = document.createElement('p');
+            const div = document.createElement('div');
+            const div2 = document.createElement('div');
+            p.innerText = seats.innerText;
+            selectedSeatList.appendChild(p);
             flag = true;
         }
     });
 
 }
+
+
+
+
+
+
+
+//on double click
+// seats.addEventListener('dblclick', function(){
+//     seats.style.backgroundColor = '#0307121A';
+//     if(flag === false){
+//         selectedSeatCounterUpdater = selectedSeatCounterUpdater;
+//         seatLeft = seatLeft;
+//     }
+//     else{
+//         selectedSeatCounterUpdater--;
+//         selectedSeatCounter.innerText = selectedSeatCounterUpdater;
+//         seatLeft++;
+//         seatLeftId.innerText = seatLeft;
+//         flag = false;
+//     }
+// });
 
