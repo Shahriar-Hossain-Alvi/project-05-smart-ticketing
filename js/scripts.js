b134bc1402f27617f2f1ebdd1231aa02fa1ed1de
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-02-18 21:52:21 +0600

    added click to change color function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
-//click banner button to go to ph paribahan section
+//f-1: click banner button to go to ph paribahan section
 const bannerBtn = document.getElementById('banner-btn');
 bannerBtn.addEventListener('click', function(){
     const phParibahan = document.getElementById('ph-paribahan');
     phParibahan.scrollIntoView();
 })
+
+//f-2: click seat to change color
+const seatLayoutContainer = document.getElementById('seat-layout-container');
+let 
+const seatID = seatLayoutContainer.querySelectorAll('h2');
+for(let i=0; i<seatID.length; i++){
+    const seats = seatID[i];
+    seats.addEventListener('click', function(){
+        seats.style.backgroundColor = 'goldenrod';
+    })
+}
+
